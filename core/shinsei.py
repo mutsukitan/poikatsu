3ad270925beb1e4d10544a0b5dad3f9c4adb6e31
Mutsuki <master@kukuserver.wtf>
2026-02-16 23:06:14 +0900

    SBI新生銀行部分の実装を終わらせました

--- core/shinsei.py
+++ core/shinsei.py
@@ -3,6 +3,7 @@ import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
 
 def shinsei(driver: uc.Chrome, config: dict) -> str:
@@ -22,39 +23,43 @@ def shinsei(driver: uc.Chrome, config: dict) -> str:
     # ログイン完了まで待機（二段階認証などがある可能性）
     WebDriverWait(driver, 240, 1).until(EC.presence_of_element_located((By.CLASS_NAME, "totalAmount")))
 
-    for i in range(config["shinsei_attempts"]):
-        # 振込ページへ移動
-        driver.execute_script("location.href='/SFC/apps/services/www/SFC/desktopbrowser/default/transfer/selectBeneficiary?mode=1';")
-        time.sleep(5)  # 念の為5秒の待機時間を設ける
+    # 振込ページへ移動
+    transfer_page_button = driver.find_elements(by=By.CLASS_NAME, value="nav-link")  # ヘッダー部分の振込ボタン
+    transfer_page_button[1].click()
+    time.sleep(5)  # 念の為5秒の待機時間を設ける
 
-        # 登録済み振込先へ振込を選ぶ
-        driver.execute_script("exeSubmitFormName('formRegtTransfer')")
+    for i in range(config["shinsei_attempts"]):
+        # 振込を行う→振込
+        driver.find_element(by=By.CSS_SELECTOR, value='a[ng-click*="TR0002"]').click()
         time.sleep(5)  # 念の為5秒の待機時間を設ける
 
         # 上から何番目の振込先かを指定し、次へ
-        driver.execute_script(f"exeSubmitFormNameArg('formNext','registNo','{config['jibun_address_order']}');")
+        transfer_list = driver.find_elements(by=By.CSS_SELECTOR, value='button[ng-click^="transfer("]')
+        transfer_list[config["shinsei_address_order"]].click()
         time.sleep(5)  # 念の為5秒の待機時間を設ける
 
         # 振込金額の入力
-        amount_input = driver.find_element(by=By.ID, value="piaAmt")
-        amount_input.send_keys(config["jibun_amount"])
+        amount_input = driver.find_element(by=By.CSS_SELECTOR, value='input[ng-model="amount"]')
+        amount_input.send_keys(config["shinsei_amount"])
 
-        # 「確認する」ボタンをクリック
-        driver.find_element(by=By.CLASS_NAME, value="js-validate-submit").click()
+        # 「次へ」をクリック
+        driver.find_element(by=By.CSS_SELECTOR, value='button[ng-click^="next("]').click()
         time.sleep(5)  # 念の為5秒の待機時間を設ける
 
-        # 振り込み実行
-        driver.execute_script("exeSubmitFormName('piaForm');")
-        time.sleep(5)  # 念の為5秒の待機時間を設ける
-        
-        for i in range(240):
-            result = driver.execute_script("return document.getElementsByClassName('c-hdg-level2')[0].innerText")
-            if result == "振込受付完了":
-                print("[+] 振り込み受付完了を確認しました。続行します。")
-                break
-            time.sleep(1)
-        else:
-            print("[-] 振り込み受付完了を確認できませんでした。10秒後に続行します。")
+        # 認証画面へ
+        # 電話認証が設定されてる場合は「実行（電話認証）」
+        driver.find_element(by=By.CLASS_NAME, value="spAuth").click()
+
+        # OTPの入力と振り込み受付完了を待機
+        try:
+            WebDriverWait(driver, 240, 1).until(EC.presence_of_element_located((By.ID, "label_transferComplete")))
+        except TimeoutException:
+            print(f"[-] 振り込み受付完了を確認できませんでした。残り振込試行回数は{config['shinsei_attempts'] - i - 1}回です。10秒後に続行します。")
             time.sleep(10)  # 次の振込まで10秒待機
+            continue
+        
+        print(f"[+] 振り込み受付完了を確認しました。残り振込試行回数は{config['shinsei_attempts'] - i - 1}回です。続行します。")
+        
+        driver.find_element(by=By.CLASS_NAME, value="TR0009PrintAndNextBtn").click()
 
     return "done"
